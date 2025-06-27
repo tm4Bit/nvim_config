d0fe9172e1c716675b42cadf08a841c526ffebe6
Tiago Araujo <tiagomuniz130@gmail.com>
2025-06-27 20:27:29 -0300

    update the angularls

--- lua/utils/lsp/servers.lua
+++ lua/utils/lsp/servers.lua
@@ -114,16 +114,15 @@ M.configurations = {
   svelte = {},
   angularls = {
     cmd = {
-      "/home/tma/.local/share/fnm/node-versions/v20.15.1/installation/bin/ngserver",
+      "/home/tma/.asdf/installs/nodejs/22.16.0/bin/ngserver",
       "--stdio",
       "--tsProbeLocations",
-      "/home/tma/.local/share/fnm/node-versions/v20.15.1/installation/lib/node_modules/@angular/language-server",
+      "/home/tma/.asdf/installs/nodejs/22.16.0/lib/node_modules/@angular/language-server",
       "--ngProbeLocations",
-      "/home/tma/.local/share/fnm/node-versions/v20.15.1/installation/lib/node_modules/@angular/language-server",
+      "/home/tma/.asdf/installs/nodejs/22.16.0/lib/node_modules/@angular/language-server",
     },
     on_new_config = function(new_config, new_root_dir)
-      local ng_lib_path =
-        "/home/tma/.local/share/fnm/node-versions/v20.15.1/installation/lib/node_modules/@angular/language-server"
+      local ng_lib_path = "/home/tma/.asdf/installs/nodejs/22.16.0/lib/node_modules/@angular/language-server"
       new_config.cmd = {
         "ngserver",
         "--stdio",
